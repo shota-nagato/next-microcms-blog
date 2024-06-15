fb00dee21e5257fb22dddbd06558787dc68579cf
shota-nagato <shota.nagato@gmail.com>
2024-06-15 11:04:56 +0900

    footer作成

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-[#333333] py-[16px] text-center text-white">
+      <span className="mt-[24px] text-xs">© Nextブログ</span>
+    </footer>
+  )
+}
